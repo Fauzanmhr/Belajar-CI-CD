04f11e8f2e6fa6e002a528c1b5695fc93cbfb7be
fauzanmhr <fauzan432003@gmail.com>
2024-08-07 16:20:02 +0700

    first test

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({
+    msg: "Hello Word"
+  })
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => console.log(`App running on PORT ${PORT}`))
+
+module.exports = app;
---
+++ index.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./index'); // Assuming your index.js exports the app
+
+describe('GET /', () => {
+  it('responds with json containing "Hello Word"', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ msg: "Coba error" });
+  });
+});
